e8b9b7688023d59d848b0f5bc5a0a73fa80c65e3
HadockHanif <hanifboss21@yahoo.com>
2016-07-13 02:27:48 +0600

    fix path issue

--- gulpfile.js
+++ gulpfile.js
@@ -10,15 +10,43 @@ const rename = require('gulp-rename');
 const browserSync = require('browser-sync');
 const runSequence = require('run-sequence');
 const del = require('del');
-const files = {
+const htmlreplace = require('gulp-html-replace');
+
+const _ = require('lodash');
+const paths = {
     html: {
-        file: 'index.html'
+        files: 'index.html',
+        srcDir: '.',
+        destDir: 'dist/'
     },
     dist: {
 
+    },
+    js: {
+        apps: 'js/app.js',
+        files: ['js/**/*.js'],
+        srcDir: 'js',
+        destDir: 'dist/assets/js'
+    },
+    templates: {
+        files: ['templates/**/*.html'],
+        srcDir: 'templates',
+        destDir: 'dist/assets/templates'
+    },
+    scss: {
+        files: ['scss/**/*.scss'],
+        srcDir: 'scss',
+        destDir: 'dist/assets/css'
     }
 };
 
+gulp.task('templates', function() {
+    return gulp.src(paths.templates.files)
+        .pipe(nunjucks())
+        .pipe(concat('templates.min.js'))
+        .pipe(streamify(uglify()))
+        .pipe(gulp.dest(paths.templates.destDir));
+});
 gulp.task('html:prod', function() {
     gulp.src('index.html')
         .pipe(gulp.dest('dist/index.html'));
@@ -37,15 +65,6 @@ gulp.task('sass', function() {
         .pipe(gulp.dest('dist/css'));
 });
 
-// Concatenate & Minify JS
-gulp.task('scripts', function() {
-    return gulp.src('js/*.js')
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('dist'))
-        .pipe(rename('all.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js'));
-});
 
 //clean dist folder
 gulp.task('clean', function() {
@@ -57,18 +76,144 @@ gulp.task('serve', function() {
         port: 5990,
         open: false,
         server: {
-            baseDir: 'dist/'
+            baseDir: 'dist/index.html'
         }
     });
 });
 
-// Watch Files For Changes
-gulp.task('watch', function() {
-    gulp.watch('js/*.js', ['lint', 'scripts']);
-    gulp.watch('scss/*.scss', ['sass']);
+gulp.task('html:dev', function() {
+    return gulp.src('./index.html')
+        .pipe(htmlreplace({
+            'app_js': './assets/js/app.js'
+        }))
+        .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build:dev', ['html:prod', 'lint', 'sass', 'scripts', 'watch']);
+gulp.task('scss', function() {
+    const scssStream = gulp.src(paths.scss.files)
+        .pipe(sass({
+            'outputStyle': 'compressed',
+            'errLogToConsole': true
+        }))
+        .pipe(concat('scss-files.scss'));
+
+    const cssextStream = gulp.src(paths.css_ext.files)
+        .pipe(concat('css-ext-files.css'));
+
+    return merge(scssStream, cssextStream)
+        .pipe(concat('style.min.css'))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest(paths.scss.destDir));
+});
+
+/* Helper which bundles the JS files and copies the bundle into the distribution file (dev) */
+function bundle(b, name) {
+    return b
+        .bundle()
+        .pipe(source(name))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        .on('error', function(error) {
+            gutil.log(gutil.colors.red('Error bundling distribution files:'), error.message);
+        })
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(paths.js.destDir));
+}
+
+/* Browserifies the JS files and copies the bundle into the distribution file (dev) */
+gulp.task('js:dev', function() {
+    glob(paths.js.apps, null, function(err, files) {
+        _.each(files, function(file) {
+            const name = file.substring(file.lastIndexOf('/') + 1);
+            const b = browserify({
+                plugin: [watchify],
+                cache: {},
+                debug: true,
+                fullPaths: true,
+                packageCache: {}
+            })
+                .transform(aliasify, {
+                    aliases: {
+                        'underscore': 'lodash'
+                    },
+                    verbose: true,
+                    global: true
+                })
+                .transform(babelify, {
+                    presets: ['es2015']
+                })
+                .transform(globalShim)
+                .add(file);
+
+            // Re-bundle the distribution file every time a source JS file changes
+            b.on('update', function() {
+                gutil.log('Re-bundling distribution files');
+                bundle(b, name);
+            });
+
+            // Log a message and reload the browser once the bundling is complete
+            b.on('log', function(message) {
+                gutil.log('Distribution files re-bundled:', message);
+                runSequence('lint:js', 'reload');
+            });
+            return bundle(b, name);
+        });
+    });
+});
+
+/* TASKS */
+/* Lints the CSS files */
+gulp.task('lint:css', function() {
+    gulp.src(paths.scss.files)
+        .pipe(sassLint())
+        .pipe(sassLint.format())
+        .pipe(sassLint.failOnError());
+});
+
+/* Compiles SCSS files into CSS files and copies them to the distribution directory */
+gulp.task('scss', function() {
+    const scssStream = gulp.src(paths.scss.files)
+        .pipe(sass({
+            'outputStyle': 'compressed',
+            'errLogToConsole': true
+        }))
+        .pipe(concat('scss-files.scss'));
+
+    const cssextStream = gulp.src(paths.css_ext.files)
+        .pipe(concat('css-ext-files.css'));
+
+    return merge(scssStream, cssextStream)
+        .pipe(concat('style.min.css'))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest(paths.scss.destDir));
+});
+
+/* Development css task */
+gulp.task('css:dev', function() {
+    runSequence('lint:css', 'scss');
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    const fileTypesToWatch = {
+        // scss: 'css:dev',
+        html: 'html:dev',
+        // fonts: 'fonts',
+        // images: 'images',
+        templates: 'templates'
+    };
+    _.forEach(fileTypesToWatch, function(taskToRun, fileType) {
+        gulp.watch(paths[fileType].files, function() {
+            runSequence(taskToRun, 'reload');
+        });
+    });
+});
+
+/* Reloads the browser */
+gulp.task('reload', function() {
+    browserSync.reload();
+});
+gulp.task('build:dev', ['html:dev', 'lint', 'sass', 'watch']);
 // Default Task
 gulp.task('default', function(done) {
     runSequence('clean', 'build:dev', 'serve', function(error) {
---
+++ js/router/router.js
@@ -0,0 +1,1 @@
+const Backbone = require('backbone');
