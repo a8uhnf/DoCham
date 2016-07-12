34491497260e6576d9cc7135cacd83fa395c6102
HadockHanif <hanifboss21@yahoo.com>
2016-07-12 22:27:06 +0600

    console

--- gulpfile.js
+++ gulpfile.js
@@ -47,7 +47,6 @@ gulp.task('scripts', function() {
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-    console.log('hello world');
     gulp.watch('js/*.js', ['lint', 'scripts']);
     gulp.watch('scss/*.scss', ['sass']);
 });
