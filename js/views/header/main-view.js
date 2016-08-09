86ea39ae40f13aa439aca5e78e2f85a5079ab9be
hanif <hanif@thetigerworks.com>
2016-08-09 22:41:46 +0600

    adding signing modal

--- js/views/header/main-view.js
+++ js/views/header/main-view.js
@@ -10,8 +10,9 @@ module.exports = Backbone.View.extend({
     this.url = 'http://127.0.0.1:3000/hello';
     this.$el.html(global.nunjucksEnv.render(this.DashboardNunj));
   },
-  logInRegister() {
-
+  logInRegister(e) {
+    e.preventDefault();
+    console.log('he;;p login/register');
   },
   render() {
     // console.log('hello renderfunction');
